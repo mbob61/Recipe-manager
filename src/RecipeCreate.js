7bf82b734ad9bafd252b1d8e423a4ea064a1625b
Mike Souster <msouster@indeed.com>
2023-12-04 11:12:15 +0000

    updated recipeCreate to use a single change handler with a recipe object, instead of multiple handlers

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -2,31 +2,23 @@ import React, { useState } from "react";
 
 function RecipeCreate({createRecipe}) {
 
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers
-  
-  const [name, setName] = useState("");
-  const [cuisine, setCuisine] = useState("");
-  const [photo, setPhoto] = useState("");
-  const [ingredients, setIngredients] = useState("");
-  const [preparation, setPreparation] = useState("");
+  const defaultState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: ""
+  }
 
-  const handleNameChange = (event) => {setName(event.target.value)}
-  const handleCuisineChange = (event) => {setCuisine(event.target.value)}
-  const handlePhotoShange = (event) => {setPhoto(event.target.value)}
-  const handleIngredientsChange = (event) => {setIngredients(event.target.value)}
-  const handlePreparationChange = (event) => {setPreparation(event.target.value)}
+  const [recipe, setRecipe] = useState(defaultState)
+  const changeHandler = (event) => {
+    setRecipe({...recipe, [event.target.name]: event.target.value })
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Submitted", name, cuisine, photo, ingredients, preparation)
-    createRecipe({name, cuisine, photo, ingredients, preparation})
-    setName("")
-    setCuisine("")
-    setPhoto("")
-    setIngredients("")
-    setPreparation("")
+    createRecipe(recipe)
+    setRecipe(defaultState)
   }
   
   return (
@@ -35,19 +27,19 @@ function RecipeCreate({createRecipe}) {
         <tbody>
           <tr>
             <td>
-              <input name="name" type="text" onChange={handleNameChange} value={name}></input>
+              <input name="name" type="text" onChange={changeHandler} value={recipe.name}></input>
             </td>
             <td>
-              <input name="cuisine" type="text" onChange={handleCuisineChange} value={cuisine}></input>
+              <input name="cuisine" type="text" onChange={changeHandler} value={recipe.cuisine}></input>
             </td>
             <td>
-              <input name="photo" type="url" onChange={handlePhotoShange} value={photo}></input>
+              <input name="photo" type="url" onChange={changeHandler} value={recipe.photo}></input>
             </td>
             <td>
-              <textarea name="ingredients" type="text" onChange={handleIngredientsChange} value={ingredients}></textarea>
+              <textarea name="ingredients" type="text" onChange={changeHandler} value={recipe.ingredients}></textarea>
             </td>
             <td>
-              <textarea name="preparation" type="text" onChange={handlePreparationChange} value={preparation}></textarea>
+              <textarea name="preparation" type="text" onChange={changeHandler} value={recipe.preparation}></textarea>
             </td>
             <td>
               <button type="submit">Create</button>
