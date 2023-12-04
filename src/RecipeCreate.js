0157bbfd429b91053308610be51428f5ed3fc3d9
Mike Souster <msouster@indeed.com>
2023-12-04 11:13:19 +0000

    put old code back for visibility

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -15,10 +15,25 @@ function RecipeCreate({createRecipe}) {
     setRecipe({...recipe, [event.target.name]: event.target.value })
   }
 
+  // const handleNameChange = (event) => {setName(event.target.value)}
+  // const handleCuisineChange = (event) => {setCuisine(event.target.value)}
+  // const handlePhotoShange = (event) => {setPhoto(event.target.value)}
+  // const handleIngredientsChange = (event) => {setIngredients(event.target.value)}
+  // const handlePreparationChange = (event) => {setPreparation(event.target.value)}
+
+  // const [name, setName] = useState("");
+  // const [cuisine, setCuisine] = useState("");
+  // const [photo, setPhoto] = useState("");
+  // const [ingredients, setIngredients] = useState("");
+  // const [preparation, setPreparation] = useState("");
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("Submitted", name, cuisine, photo, ingredients, preparation)
     createRecipe(recipe)
     setRecipe(defaultState)
+    // setName("");
   }
   
   return (
